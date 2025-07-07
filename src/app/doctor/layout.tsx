79d2ff9b0bdf1c49c01e2e43198b021957a694cb
Vaibhav Lakhwani <vaibhavlakhwanivns@gmail.com>
2025-07-07 07:24:29 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="space-

--- src/app/doctor/layout.tsx
+++ src/app/doctor/layout.tsx
@@ -6,7 +6,11 @@ import { DashboardLayout } from "@/components/layout/DashboardLayout";
 import { doctorSidebarNav } from "@/lib/constants";
 
 function DoctorLayout({ children }: { children: React.ReactNode }) {
-  return <DashboardLayout navItems={doctorSidebarNav}>{children}</DashboardLayout>;
+  return (
+    <DashboardLayout navItems={doctorSidebarNav}>
+      <div className="p-4 sm:p-6 lg:p-8">{children}</div>
+    </DashboardLayout>
+  );
 }
 
 export default withAuth(DoctorLayout, ["Doctor"]);
