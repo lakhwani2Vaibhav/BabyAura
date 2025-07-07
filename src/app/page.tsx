02c3fd53d0c059baba4df68a1fe397595828d35f
Vaibhav Lakhwani <vaibhavlakhwanivns@gmail.com>
2025-07-07 12:26:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,7 +111,7 @@ export default function Home() {
       <MarketingHeader />
       <main className="flex-1">
         <AnimatedContent>
-          <section className="w-full pt-24 pb-12 md:pt-36 md:pb-24 lg:pt-44 lg:pb-32 xl:pt-60 xl:pb-48">
+          <section className="w-full pt-20 pb-12 md:pt-28 md:pb-24 lg:pt-32 lg:pb-32 xl:pt-40 xl:pb-48">
             <div className="container px-4 md:px-6">
               <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
                 <div className="flex flex-col justify-center space-y-4">
