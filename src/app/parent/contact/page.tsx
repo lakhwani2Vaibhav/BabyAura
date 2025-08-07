acd83901645c9877e457678c3b7d674669a5ee88
BabyAura <babyauraindia@gmail.com>
2025-08-07 15:28:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/parent/contact/page.tsx
+++ src/app/parent/contact/page.tsx
@@ -6,6 +6,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
   Card,
+  CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
