d7ee9c1b6ab25df368c07bddd72cffe95874e285
Vaibhav Lakhwani <vaibhavlakhwanivns@gmail.com>
2025-08-23 19:57:25 +0000

    in home page u have added bug things are scrollable in parent page

--- src/app/parent/layout.tsx
+++ src/app/parent/layout.tsx
@@ -9,7 +9,7 @@ function ParentLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col h-screen bg-background text-foreground">
       <ParentHeader />
-      <main className="container mx-auto px-4 pt-20 pb-4 flex-1 overflow-y-auto md:overflow-y-hidden">
+      <main className="container mx-auto px-4 pt-20 pb-4 flex-1 overflow-y-auto">
         {children}
       </main>
     </div>
